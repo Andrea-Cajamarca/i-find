f4c64d946a296b895925c0e774c96db6665bf92d
andrea_cajamarca <andreakcajamarca@gmail.com>
2017-04-05 22:37:07 +0000

    changing font size for categories

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 /**
  * script.js
  * - All our useful JS goes here, awesome!
- */
-
- 
+//
+idea for Trailers API
+--http://api.traileraddict.com/?film=curious-case-benjamin-button&count=3
+*/ 
 
 $(document).ready(function() {
 ///
- console.log("Ready");
+  $("form").hide();
   $("#button").click(compileMovies); 
   function compileMovies() {
 
@@ -16,6 +17,7 @@ $(document).ready(function() {
       "https://www.omdbapi.com/?t=" + $("input[name='title']").val() ,
      
       function(response) {
+        $("form").show();
         console.log(response);
         $("#poster").html("<img src=" + response.Poster + ">");
         $("#actors").html(response.Actors);
@@ -23,7 +25,8 @@ $(document).ready(function() {
         $("#genre").html(response.Genre);
         $("#year").html(response.Year);
         $("#country").html(response.Country);
-
+        $("#language").html(response.Language);
+        $("#director").html(response.Director);
       });
   }
 });
